ce0ce9cd4e6263225d461dea8a8f7aefc49a169e
Vadim Zaliva <lord@crocodile.org>
2017-01-15 10:02:36 -0800

    using python API

--- scripts/set_lang.py
+++ scripts/set_lang.py
@@ -12,28 +12,30 @@
 
 import subprocess
 import sys
+from gi.repository import Gio
 
-LAST='/dev/shm/last-input.txt'
-K = ["org.gnome.desktop.input-sources", "current"]
+LAST = '/dev/shm/last-input.txt'
+SCHEMA = 'org.gnome.desktop.input-sources'
+KEY = 'current'
 
 def read_last():
     try:
         with open(LAST, 'r') as f:
-            return f.read().split(',')
+            return tuple(map(int, f.read().split(',')))
     except:
-        return ('0','0')
+        return (0,0)
 
 def write_last(v0,v1):
     with open(LAST, 'w') as f:
-        return f.write(str.join(",",(v0,v1)))
+        return f.write(str.join(",",(str(v0),str(v1))))
 
-def get(command):
-    return subprocess.check_output(command).decode("utf-8")
+g = Gio.Settings.new(SCHEMA)
 
-c = get(["gsettings", "get", K[0], K[1]]).strip().split()[-1]
+c = g.get_uint(KEY)
 (l,c0) = read_last() # read previous transition l->c0
+print(c,l,c0)
 if c0 != c:
     l = c0 # switch via menu detected
-subprocess.Popen(["gsettings", "set", K[0], K[1], l])
+g.set_uint(KEY,l)
 write_last(c,l) # write current transition c->l
 
