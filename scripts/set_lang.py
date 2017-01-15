5ee88dcfb6520cb67749f3efe2d2b7b43b2a8ffb
Vadim Zaliva <lord@crocodile.org>
2017-01-15 09:33:44 -0800

    gracefully handle swtich outside script

--- scripts/set_lang.py
+++ scripts/set_lang.py
@@ -19,20 +19,21 @@ K = ["org.gnome.desktop.input-sources", "current"]
 def read_last():
     try:
         with open(LAST, 'r') as f:
-            return f.read()
+            return f.read().split(',')
     except:
-        return '0'
+        return ('0','0')
 
-def write_last(v):
+def write_last(v0,v1):
     with open(LAST, 'w') as f:
-        return f.write(v)
-
+        return f.write(str.join(",",(v0,v1)))
 
 def get(command):
     return subprocess.check_output(command).decode("utf-8")
 
-currlang = get(["gsettings", "get", K[0], K[1]]).strip().split()[-1]
-lastlang = read_last()
-subprocess.Popen(["gsettings", "set", K[0], K[1], lastlang])
-write_last(currlang)
+c = get(["gsettings", "get", K[0], K[1]]).strip().split()[-1]
+(l,c0) = read_last() # read previous transition l->c0
+if c0 != c:
+    l = c0 # switch via menu detected
+subprocess.Popen(["gsettings", "set", K[0], K[1], l])
+write_last(c,l) # write current transition c->l
 
