dc7a3f75ef953d64224de5dedb68fa2edcd032e8
Vadim Zaliva <lord@crocodile.org>
2017-01-15 17:47:43 -0800

    heuristic to avoid get stuck

--- scripts/set_lang.py
+++ scripts/set_lang.py
@@ -32,10 +32,26 @@ def write_last(v0,v1):
 g = Gio.Settings.new(SCHEMA)
 
 c = g.get_uint(KEY)
-(l,c0) = read_last() # read previous transition l->c0
-print(c,l,c0)
+# read previous transition l->c0
+(l,c0) = read_last() 
+
 if c0 != c:
-    l = c0 # switch via menu detected
+    # switch via menu detected
+    l = c0
+
+if l == c:
+    # we are stuck!
+    if l != 0:
+        # if we stuck at non-0, switching to source 0
+        # which is assumed to be always present is an alternative
+        l = 0
+    else:
+        # if we stuck at source #0 try to pick up an alternative
+        # see if we have more than one, and switch to #1
+        if len(g.get_value('sources')) > 1:
+            l = 1
+            
 g.set_uint(KEY,l)
-write_last(c,l) # write current transition c->l
+# write current transition c->l
+write_last(c,l) 
 
