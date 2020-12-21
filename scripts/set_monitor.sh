d94b85070d0b9122a131d97303f921f1128be47f
Vadim Zaliva <lord@crocodile.org>
2020-12-21 10:27:28 -0800

    landscape

--- scripts/set_monitor.sh
+++ scripts/set_monitor.sh
@@ -4,7 +4,7 @@ if `xrandr | grep -q "HDMI-1 connected 3840x2160+0+0"`; then
     # Secondary UDH display in landscape mode
     # set it above main one
     xrandr > /dev/null
-    xrandr --output HDMI-1 --auto --right-of eDP-1 --rotate left --dpi 200 --noprimary
+    xrandr --output HDMI-1 --auto --above eDP-1 --rotate normal
     xrandr --dpi 96/e-DP1
     xrandr --output eDP-1 --primary
 else
