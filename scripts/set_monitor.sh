78f21af603f9c386a066a9a1957e176a03cbc1be
Vadim Zaliva <lord@crocodile.org>
2021-01-22 15:41:36 -0800

    enforece resolution

--- scripts/set_monitor.sh
+++ scripts/set_monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if `xrandr | grep -q "HDMI-1 connected 3840x2160+0+0"`; then
-    # Secondary UDH display in landscape mode
+    # Secondary UHD display in landscape mode
     # set it above main one
     xrandr > /dev/null
     xrandr --output HDMI-1 -s 3840x2160 --above eDP-1 --rotate normal
