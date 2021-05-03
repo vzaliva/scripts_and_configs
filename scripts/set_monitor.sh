54541159b26302699fff0db44d35ad05e77751f1
Vadim Zaliva <lord@crocodile.org>
2021-05-03 10:23:10 -0700

    support for ThinkPad USB-C Dock Gen2

--- scripts/set_monitor.sh
+++ scripts/set_monitor.sh
@@ -7,6 +7,14 @@ if `xrandr | grep -q "HDMI-1 connected 3840x2160+0+0"`; then
     xrandr --output HDMI-1 -s 3840x2160 --above eDP-1 --rotate normal
     xrandr --dpi 96/e-DP1
     xrandr --output eDP-1 --primary
+elif `xrandr | grep -q "DP-1-1 connected"`; then
+    # Secondary UHD display in landscape mode
+    # set it above main one
+    xrandr > /dev/null
+    xrandr --output DP-1-1 --auto
+    xrandr --output DP-1-1 -s 3840x2160 --above eDP-1 --rotate normal
+    xrandr --dpi 96/e-DP1
+    xrandr --output eDP-1 --primary
 else
     # No secondary
     xrandr --auto
