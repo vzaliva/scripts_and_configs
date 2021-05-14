8696a53ab6daff7ea936fce6cc09aafa35d0ae08
Vadim Zaliva <lord@crocodile.org>
2021-05-14 10:41:08 -0700

    support 2nd USB-c port

--- scripts/set_monitor.sh
+++ scripts/set_monitor.sh
@@ -15,6 +15,14 @@ elif `xrandr | grep -q "DP-1-1 connected"`; then
     xrandr --output DP-1-1 -s 3840x2160 --above eDP-1 --rotate normal
     xrandr --dpi 96/e-DP1
     xrandr --output eDP-1 --primary
+elif `xrandr | grep -q "DP-2-1 connected"`; then
+    # Secondary UHD display in landscape mode
+    # set it above main one
+    xrandr > /dev/null
+    xrandr --output DP-2-1 --auto
+    xrandr --output DP-2-1 -s 3840x2160 --above eDP-1 --rotate normal
+    xrandr --dpi 96/e-DP1
+    xrandr --output eDP-1 --primary
 else
     # No secondary
     xrandr --auto
