6f427a67f8e6cd326e2f3b37355002a02ec327bc
Vadim Zaliva <lord@crocodile.org>
2021-06-09 10:28:00 -0700

    moving workspaces to external monitor

--- scripts/set_monitor.sh
+++ scripts/set_monitor.sh
@@ -7,6 +7,8 @@ if `xrandr | grep -q "HDMI-1 connected 3840x2160+0+0"`; then
     xrandr --output HDMI-1 -s 3840x2160 --above eDP-1 --rotate normal
     xrandr --dpi 96/e-DP1
     xrandr --output eDP-1 --primary
+    # move workspaces 7,8 to external monitor
+    i3-msg -q "workspace 7; move workspace to output HDMI-1; workspace 8; move workspace to output HDMI-1"
 elif `xrandr | grep -q "DP-1-1 connected"`; then
     # Secondary UHD display in landscape mode
     # set it above main one
@@ -15,6 +17,8 @@ elif `xrandr | grep -q "DP-1-1 connected"`; then
     xrandr --output DP-1-1 -s 3840x2160 --above eDP-1 --rotate normal
     xrandr --dpi 96/e-DP1
     xrandr --output eDP-1 --primary
+    # move workspaces 7,8 to external monitor
+    i3-msg -q "workspace 7; move workspace to output DP-1-1; workspace 8; move workspace to output DP-1-1"
 elif `xrandr | grep -q "DP-2-1 connected"`; then
     # Secondary UHD display in landscape mode
     # set it above main one
@@ -23,8 +27,12 @@ elif `xrandr | grep -q "DP-2-1 connected"`; then
     xrandr --output DP-2-1 -s 3840x2160 --above eDP-1 --rotate normal
     xrandr --dpi 96/e-DP1
     xrandr --output eDP-1 --primary
+    # move workspaces 7,8 to external monitor
+    i3-msg -q "workspace 7; move workspace to output DP-2-1; workspace 8; move workspace to output DP-2-1"
 else
     # No secondary
     xrandr --auto
+    # move workspaces 7,8 to back primary monitor
+    i3-msg -q "workspace 7; move workspace to output eDP-1; workspace 8; move workspace to output eDP-1"
 fi
 pacmd set-default-sink 4    
