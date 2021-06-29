a6a6d78ca5c1fb505eb0d891ef07fc75f74d09d7
Vadim Zaliva <lord@crocodile.org>
2021-06-29 10:46:35 -0700

    fish config update

--- configs/dot_config/fish/config.fish
+++ configs/dot_config/fish/config.fish
@@ -31,6 +31,11 @@ end
 set -gx MANPATH (/usr/bin/manpath -g) $MANPATH
 
 set -g theme_display_date no
+#set -g theme_powerline_fonts no
+set -g theme_display_git_default_branch yes
+set -g theme_git_default_branches master main
+set -g theme_title_use_abbreviated_path yes
+set -g theme_color_scheme solarized
 
 function fish_right_prompt; end
 
