a05ea5d774378571b4c5c36a28ba9f9dec481a5b
Vadim Zaliva <lord@crocodile.org>
2023-07-31 10:48:57 -0700

    more flexbile monitor detection. TODO: resolution match

--- scripts/set_monitor.sh
+++ scripts/set_monitor.sh
@@ -1,38 +1,48 @@
-#!/bin/sh
+#!/bin/bash
 
-if `xrandr | grep -q "HDMI-1 connected 3840x2160"`; then
-    # Secondary UHD display in landscape mode
-    # set it above main one
-    xrandr > /dev/null
-    xrandr --output HDMI-1 -s 3840x2160 --above eDP-1 --rotate normal
-    xrandr --dpi 96/e-DP1
-    xrandr --output eDP-1 --primary
-    # move workspaces 7,8 to external monitor
-    i3-msg -q "workspace 7; move workspace to output HDMI-1; workspace 8; move workspace to output HDMI-1"
-elif `xrandr | grep -q "DP-1-1 connected"`; then
-    # Secondary UHD display in landscape mode
-    # set it above main one
-    xrandr > /dev/null
-    xrandr --output DP-1-1 --auto
-    xrandr --output DP-1-1 -s 3840x2160 --above eDP-1 --rotate normal
-    xrandr --dpi 96/e-DP1
-    xrandr --output eDP-1 --primary
-    # move workspaces 7,8 to external monitor
-    i3-msg -q "workspace 7; move workspace to output DP-1-1; workspace 8; move workspace to output DP-1-1"
-elif `xrandr | grep -q "DP-2-1 connected"`; then
-    # Secondary UHD display in landscape mode
-    # set it above main one
-    xrandr > /dev/null
-    xrandr --output DP-2-1 --auto
-    xrandr --output DP-2-1 -s 3840x2160 --above eDP-1 --rotate normal
-    xrandr --dpi 96/e-DP1
-    xrandr --output eDP-1 --primary
-    # move workspaces 7,8 to external monitor
-    i3-msg -q "workspace 7; move workspace to output DP-2-1; workspace 8; move workspace to output DP-2-1"
-else
+# Flag for verbose mode
+verbose=false
+
+# Check for -v command line option
+if [[ $1 == "-v" ]]; then
+    verbose=true
+fi
+
+DP=""
+
+# Save xrandr output
+xrandr_output=$(xrandr)
+
+# Read xrandr output line by line
+while IFS= read -r line
+do
+    # Use regex to find lines that match "HDMI-?" or "DP-?-?" followed by "connected"
+    if [[ $line =~ (HDMI-[0-9]+|DP-[0-9]-[0-9]+)(\ connected) ]]; then
+        # Echo the matched name
+        DP=${BASH_REMATCH[1]}
+        break
+    fi
+done <<<"$xrandr_output"
+
+# If no match was found, execute some other code
+if [ -z "$DP" ]; then
+    if [ "$verbose" = true ]; then
+        echo "No secondry monitor found"
+    fi
     # No secondary
     xrandr --auto
     # move workspaces 7,8 to back primary monitor
     i3-msg -q "workspace 7; move workspace to output eDP-1; workspace 8; move workspace to output eDP-1"
+else
+    if [ "$verbose" = true ]; then
+        echo "Found external monitor $DP"
+    fi
+    xrandr --output $DP --auto
+    xrandr --output $DP -s 3840x2160 --above eDP-1 --rotate normal
+    xrandr --dpi 96/e-DP1
+    xrandr --output eDP-1 --primary
+    # move workspaces 7,8 to external monitor
+    i3-msg -q "workspace 7; move workspace to output $DP; workspace 8; move workspace to output $DP"
 fi
+    
 #pacmd set-default-sink 4    
