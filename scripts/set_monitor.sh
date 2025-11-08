fc6f3b2ea257d22faef548206c9f3323003f567f
Vadim Zaliva <lord@crocodile.org>
2025-11-07 17:00:43 -0800

    21RVCTO1WW ThinkPad P14s Gen 6 AMD fixes

--- scripts/set_monitor.sh
+++ scripts/set_monitor.sh
@@ -17,7 +17,7 @@ xrandr_output=$(xrandr)
 while IFS= read -r line
 do
     # Use regex to find lines that match "HDMI-?" or "DP-?-?" followed by "connected"
-    if [[ $line =~ ^(HDMI-[0-9]+|DP-[0-9]+(-[0-9]+)?)\ connected ]]; then
+    if [[ $line =~ ^(HDMI-[0-9]+|DisplayPort-[0-9]+(-[0-9]+)?)\ connected ]]; then
         # Echo the matched name
         DP=${BASH_REMATCH[1]}
         break
@@ -32,15 +32,15 @@ if [ -z "$DP" ]; then
     # No secondary
     xrandr --auto
     # move workspaces 7,8 to back primary monitor
-    i3-msg -q "workspace 7; move workspace to output eDP-1; workspace 8; move workspace to output eDP-1"
+    i3-msg -q "workspace 7; move workspace to output eDP; workspace 8; move workspace to output eDP"
 else
     if [ "$verbose" = true ]; then
         echo "Found external monitor $DP"
     fi
     xrandr --output $DP --auto
-    xrandr --output $DP -s 3840x2160 --above eDP-1 --rotate normal
-    xrandr --dpi 96/e-DP1
-    xrandr --output eDP-1 --primary
+    xrandr --output $DP -s 3840x2160 --above eDP --rotate normal
+    xrandr --dpi 96/eDP
+    xrandr --output eDP --primary
     # move workspaces to external monitor
     workspace_numbers=( 6 7 8 9 10 )
     for workspace in "${workspace_numbers[@]}"; do
@@ -49,21 +49,21 @@ else
 fi
 
 # List sinks and search for the desired one by name, then extract its ID
-SINK_ID=$(pactl list short sinks | egrep "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic" | cut -f2)
+#SINK_ID=$(pactl list short sinks | egrep "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic" | cut -f2)
 
 # Check if the SINK_ID was found
-if [ -n "$SINK_ID" ]; then
-    # Set the default sink to your desired sink
-    pactl set-default-sink "$SINK_ID"
-    if [ "$verbose" = true ]; then
-        echo "Default sink set to ID $SINK_ID."
-    fi
-else
-    if [ "$verbose" = true ]; then
-        echo "No sink matching criteria found."
-    fi
-fi
+#if [ -n "$SINK_ID" ]; then
+#    # Set the default sink to your desired sink
+#    pactl set-default-sink "$SINK_ID"
+#    if [ "$verbose" = true ]; then
+#        echo "Default sink set to ID $SINK_ID."
+#    fi
+#else
+#    if [ "$verbose" = true ]; then
+#        echo "No sink matching criteria found."
+#    fi
+#fi
 
-~/bin/set_sound.py
+#~/bin/set_sound.py
 
 
